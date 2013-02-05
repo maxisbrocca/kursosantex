44d6d863661fceeae53be6734f94044f5d178584
efeser <erwin.feser@gmail.com>
2013-02-05 10:45:17 -0300

    dajaxice ready!

--- kursoapp/ajax.py
+++ kursoapp/ajax.py
@@ -1,6 +1,16 @@
 from django.utils import simplejson
 from dajaxice.decorators import dajaxice_register
+import random
+from dajax.core import Dajax
 
 @dajaxice_register
 def sayhello(request):
     return simplejson.dumps({'message':'Hello World'})
+
+
+
+@dajaxice_register
+def randomize(request):
+    dajax = Dajax()
+    dajax.assign('#result', 'value', random.randint(1, 10))
+    return dajax.json()

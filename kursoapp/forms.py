55d04913c25d64df6b86416543caf0b99032695b
efeser <erwin.feser@gmail.com>
2013-02-08 11:10:22 -0300

    form generico

--- kursoapp/forms.py
+++ kursoapp/forms.py
@@ -3,10 +3,30 @@ from django import forms
 from models import BlogPost, Cliente, Diapositiva, HomePost, RespuestaBlogPost, Servicio
 
 class RespuestaBlogPostForm(forms.ModelForm):
-
     class Meta:
         model = RespuestaBlogPost
+
 
 class HomePostForm(forms.ModelForm):
     class Meta:
         model = HomePost
+
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = BlogPost
+
+
+class ServicioForm(forms.ModelForm):
+    class Meta:
+        model = Servicio
+
+
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+
+
+class DiapositivaForm(forms.ModelForm):
+    class Meta:
+        model = Diapositiva
--- kursoapp/views.py
+++ kursoapp/views.py
@@ -3,8 +3,15 @@ from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from forms import HomePostForm
+from forms import HomePostForm, BlogPostForm, ClienteForm, RespuestaBlogPostForm, DiapositivaForm, ServicioForm
 from models import HomePost
+
+classnames = {'homepost': HomePostForm,
+              'blogpost': BlogPostForm,
+              'respuestablogpost': RespuestaBlogPostForm,
+              'cliente': ClienteForm,
+              'diapositiva': DiapositivaForm,
+              'servicio': ServicioForm}
 
 @login_required
 def home(request):
@@ -15,13 +22,16 @@ def home(request):
 
 
 @login_required
-def new_home_post(request):
+def new(request, classname):
+    if classname not in classnames.keys():
+        return HttpResponseRedirect(reverse('home_view'))
+    formclass = classnames.get(classname)
     if request.method == 'POST':
-        form = HomePostForm(request.POST, request.FILES)
+        form = formclass(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('home_view'))
     else:
-        form = HomePostForm()
-    return render_to_response('homepost.html', {'form': form},
+        form = formclass()
+    return render_to_response('modelform.html', {'form': form},
         context_instance=RequestContext(request))
